171fd34fed944e2cbeec2e96be3d4fd17196abe9
you@example.com <you@example.com>
2026-06-01 14:46:29 -0300

    feat: 01/06/2026

--- sos/app/src/main/java/dev/guilherme/sos/MainActivity.kt
+++ sos/app/src/main/java/dev/guilherme/sos/MainActivity.kt
@@ -1,12 +1,25 @@
 package dev.guilherme.sos
 
+import android.Manifest
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textContactInfo: TextView
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,62 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Inicializando o serviço SharedPreferences
+        sharedPreferences = getSharedPreferences("soccoro", MODE_PRIVATE)
+
+        //ligação entre o jotlin e o XMl
+        textContactInfo = findViewById(R.id.textContactInfo)
+
+        // ========= botão config ===========
+        findViewById<ImageButton>(R.id.imageButtonConfig).setOnClickListener{
+            openConfigActivity()
+        }
+
+        // ========= botão sos =======
+        findViewById<Button>(R.id.buttonSos).setOnClickListener{
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+
+            } else {
+
+            }
+        }
+    }//fim do OnCreate
+
+    override fun onStart() {
+        super.onStart()
+        initSetup()
     }
-}
+
+    private fun displayContactInfo(){
+        val contactName = sharedPreferences.getString("contactName", null)
+        val contactPhone = sharedPreferences.getString("contactPhone", null)
+        textContactInfo.setText("$contactName | $contactPhone")
+    }
+
+    private fun initSetup(){
+        //verificar se a propriedade "contactPhone" existe
+        //se sim então mostre na tela, se não exiba o pop-up
+        if (sharedPreferences.contains("contactPhone")) {
+            displayContactInfo()
+        }else{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Bem vindo(a) ao App Socorro!")
+            builder.setMessage("This is a simple alert message.")
+            builder.setPositiveButton("Configurar agora"){dialog, which ->
+                openConfigActivity()
+            }
+            builder.create()
+            builder.show()
+        }
+    }//fim do initSetup
+
+    private fun openConfigActivity(){
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+    }//fim do openConfigActivity
+
+}//fim da classe
--- sos/app/src/main/java/dev/guilherme/sos/SettingsActivity.kt
+++ sos/app/src/main/java/dev/guilherme/sos/SettingsActivity.kt
@@ -1,12 +1,25 @@
 package dev.guilherme.sos
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
+
+    //Declaração dos atributos da classe
+    private lateinit var editTextContactName: EditText
+    private lateinit var editTextContactPhone: EditText
+    private lateinit var editTextCustomMsg: EditText
+
+    //Armazena valores que serão disponíveis em todo o APP
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +28,39 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-}
+        }//fim do ViewCompat
+
+        //Inicializando o serviço SharedPreferences
+        sharedPreferences = getSharedPreferences("socorro", MODE_PRIVATE)
+
+        //Ligação entre o kotlin e o XML
+        editTextContactName = findViewById(R.id.TextInputEditTextContactName)
+        editTextContactPhone = findViewById(R.id.TextInputEditTextContactPhone)
+        editTextCustomMsg = findViewById(R.id.TextInputEditTextCustomMessage)
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            savePreferences()
+        }//fim do buttonSave
+
+        //MELHORAR ISSO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        loadPreferences()
+
+    }//fim do onCreate
+
+    private fun savePreferences(){
+        sharedPreferences.edit()
+            .putString("contactName",editTextContactName.text.toString())
+            .putString("contactPhone",editTextContactPhone.text.toString())
+            .putString("msg", editTextCustomMsg.text.toString())
+            .apply()
+        Toast.makeText(this, "Susesso!!!", Toast.LENGTH_SHORT).show()
+        loadPreferences()
+    }//fim do savePreferences
+
+    private fun loadPreferences(){
+        editTextContactName.setText(sharedPreferences.getString("contactName",""))
+        editTextContactPhone.setText(sharedPreferences.getString("contactPhone",""))
+        editTextCustomMsg.setText(sharedPreferences.getString("msg",""))
+    }//fim do loadPreferences
+
+}//fim da classe
